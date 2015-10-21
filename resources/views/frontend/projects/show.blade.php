d727a7924ae51ef9aee996469b24bf7a97d79041
Rafael Slonik <rslonik@gmail.com>
2015-10-21 21:04:53 -0200

    Atualização layout Projetos, informações fixas para exemplo

--- resources/views/frontend/projects/show.blade.php
+++ resources/views/frontend/projects/show.blade.php
@@ -51,6 +51,12 @@
             <li class="list-group-item"><a href="#">Contribuidores</a></li>
             <li class="list-group-item"><a href="#">Comentário</a></li>
           </ul>
+          <ul class="list-unstyled">
+            <li class="link-external"><a href="#">Informações</a></li>
+            <li class="link-external"><a href="#">Atividade</a></li>
+            <li class="link-external"><a href="#">Contribuidores</a></li>
+            <li class="link-external"><a href="#">Comentário</a></li>
+          </ul>
         </div>
         <div class="content-project col-sm-9">
           <div class="project-details">
@@ -64,22 +70,107 @@
           <div class="description">
             <p>{{ $project->description }}</p>
           </div>
+
+          <!-- PROJECT PANELS -->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Ingredientes Faltando</h3>
+            </div>
+            <div class="panel-body">
+              Panel content
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Fazer...</h3>
+            </div>
+            <div class="panel-body">
+              Panel content
+            </div>
+          </div>
+          <!-- /PROJECT PANELS -->
+
+          <!-- PEOPLE -->
+          <div class="people">
+            <span><strong>16</strong> CONTRIBUIDORES</span>
+            <ul class="list-unstyled list-inline">
+              <li class="contrib-person"><a href="#"><img src="https://secure.gravatar.com/avatar/61bde54bc933a749f0b706b0dbb31c8c?d=https%3A%2F%2Fwalter.trakt.us%2Fplaceholders%2Fmedium%2Ffry.png&r=pg&s=256"/></a></li>
+              <li class="contrib-person"><a href="#"><img src="https://secure.gravatar.com/avatar/61bde54bc933a749f0b706b0dbb31c8c?d=https%3A%2F%2Fwalter.trakt.us%2Fplaceholders%2Fmedium%2Ffry.png&r=pg&s=256"/></a></li>
+              <li class="contrib-person"><a href="#"><img src="https://secure.gravatar.com/avatar/61bde54bc933a749f0b706b0dbb31c8c?d=https%3A%2F%2Fwalter.trakt.us%2Fplaceholders%2Fmedium%2Ffry.png&r=pg&s=256"/></a></li>
+              <li class="contrib-person"><a href="#"><img src="https://secure.gravatar.com/avatar/61bde54bc933a749f0b706b0dbb31c8c?d=https%3A%2F%2Fwalter.trakt.us%2Fplaceholders%2Fmedium%2Ffry.png&r=pg&s=256"/></a></li>
+            </ul>
+          </div>
+          <!-- /PEOPLE -->
+
+          <!-- TEAM -->
+          <div class="team">
+            <h3>Time</h3>
+            <ul class="list-unstyled list-inline">
+              <li class="team-person"><img src="https://trakt.tv/assets/placeholders/thumb/poster-2d5709c1b640929ca1ab60137044b152.png"/><a href="#">Stephan Garcia</a></li>
+              <li class="team-person"><img src="https://trakt.tv/assets/placeholders/thumb/poster-2d5709c1b640929ca1ab60137044b152.png"/><a href="#">Stephan Garcia</a></li>
+              <li class="team-person"><img src="https://trakt.tv/assets/placeholders/thumb/poster-2d5709c1b640929ca1ab60137044b152.png"/><a href="#">Stephan Garcia</a></li>
+              <li class="team-person"><img src="https://trakt.tv/assets/placeholders/thumb/poster-2d5709c1b640929ca1ab60137044b152.png"/><a href="#">Stephan Garcia</a></li>
+              <li class="team-person"><img src="https://trakt.tv/assets/placeholders/thumb/poster-2d5709c1b640929ca1ab60137044b152.png"/><a href="#">Stephan Garcia</a></li>
+            </ul>
+          </div>
+          <!-- /TEAM -->
+
+          <!-- COMMENT -->
+          <div class="comments">
+            <h3>Comentários</h3>
+            <ul class="list-unstyled">
+              <li class="comment">
+                <i class="fa fa-comment-o col-sm-2"></i>
+                <div class="col-sm-10">
+                  <p><strong>Nima Kazerooni</strong></p>
+                  <p>Que massa, cara!</p>
+                </div>
+              </li>
+              <li class="comment">
+                <i class="fa fa-comment-o col-sm-2"></i>
+                <div class="col-sm-10">
+                  <p><strong>Rodrigo Alvarenga</strong></p>
+                  <p>Cara, puta que projeto que massa.</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <!-- /COMMENT -->
+
+
         </div>
+
       </div>
+
+      <!-- OTHER PROJECTS -->
+      <div class="other-projects">
+        <h3>Conheça outros projetos</h3>
+        <ul class="list-unstyled list-inline">
+          <li class="related-project">
+            <a href="#"><img src="https://walter.trakt.us/images/movies/000/000/009/fanarts/thumb/96364d5e6d.jpg"/>
+              <span class="other-projects-title">Vizin</span>
+            </a>
+            <span class="loves"><i class="fa fa-heart"></i> <strong>60%</strong></span>
+          </li>
+          <li class="related-project">
+            <a href="#"><img src="https://walter.trakt.us/images/movies/000/000/009/fanarts/thumb/96364d5e6d.jpg"/>
+              <span class="other-projects-title">Vizin</span>
+            </a>
+            <span class="loves"><i class="fa fa-heart"></i> <strong>60%</strong></span>
+          </li>
+        </ul>
+      </div>
+      <!-- /OTHER PROJECTS -->
+
+      <!-- SEARCH PROJECTS -->
+      <div class="search-projects">
+        <h3>Encontre um projeto</h3>
+        <input type="text" class="form-control" placeholder="Busque por Vizin, Kartão ou outros..."/>
+      </div>
+      <!-- /OTHER PROJECTS -->
     </section>
     <!-- /PROJECT DESCRIPTION -->
 
-    <!-- PEOPLE -->
-
-    <section class="row">
-      <div class="container">
-        <div class="people">
-          <span><strong>16</strong> PEOPLE IN THIS PROJECT</span>
-        </div>
-      </div>
-    </section>
-
-    <!-- /PEOPLE -->
 
 @endsection
 
